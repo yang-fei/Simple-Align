fffe72e15385db3166f737e3110991b1bc29a28f
Dreamzz5 <46215105+Dreamzz5@users.noreply.github.com>
2025-03-17 17:46:05 +0800

    Update realesrnet_model.py

--- realesrgan/models/realesrnet_model.py
+++ realesrgan/models/realesrnet_model.py
@@ -49,11 +49,10 @@ class RealESRNetModel(SRModel):
                     self.kernel1 = data['kernel1'].to(self.device)
                     self.kernel2 = data['kernel2'].to(self.device)
                     self.sinc_kernel = data['sinc_kernel'].to(self.device)
-                # else:
-                #     self.kernel1 = data['kernel3'].to(self.device)
-                #     self.kernel2 = data['kernel4'].to(self.device)
-                #     self.sinc_kernel = data['sinc_kernel2'].to(self.device)
-                # print(self.kernel1.shape, self.kernel2.shape, self.sinc_kernel.shape)
+                else:
+                     self.kernel1 = data['kernel3'].to(self.device)
+                     self.kernel2 = data['kernel4'].to(self.device)
+                     self.sinc_kernel = data['sinc_kernel2'].to(self.device)
                 # ----------------------- The first degradation process ----------------------- #
                 # blur
                 out = filter2D(self.gt, self.kernel1)
